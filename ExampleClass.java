880865dcead2f988fc9b96be644a7db373c997e9
LocallySourcedBuggy-stack1221 <80067709+LocallySourcedBuggy-stack1221@users.noreply.github.com>
2021-03-07 14:17:16 -0800

    njdvlkswvd
    jelknjes

--- ExampleClass.java
+++ ExampleClass.java
@@ -2,6 +2,14 @@ package com.simpleexample;
 
 public class ExampleClass {
     public static void main(String[] args) {
+        System.out.println("Hello World// heuihfuoehfoiehifew
+package com.zacharylavoie.example_package;
+
+public class ExampleClass {
+    public static void main(String[] args) {
         System.out.println("Hello World!");
+    }
+}
+!");
     }
 }
