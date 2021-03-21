7925ff297c282191935267f655413d3c1f8062b8
LocallySourcedBuggy-stack1221 <80067709+LocallySourcedBuggy-stack1221@users.noreply.github.com>
2021-03-20 20:35:51 -0700

    OneHundredFiftyCCEngine

---
+++ OneHundredFiftyCCEngine.java
@@ -0,0 +1,83 @@
+/* Emolyn Tumwebaze
+   3/14/2021
+   This class creates a OneHundredFiftyCCEngine as an option for the user to pick and play the game.
+* */
+
+public class OneHundredFiftyCCEngine extends Engine {
+
+    private double SPEED_THRESHOLD;     // Represents how fast a car can move
+    private double POWER_THRESHOLD;     // Represents the maximum output torque of a given 150 cc engine.
+
+    // Default Constructor
+    public OneHundredFiftyCCEngine() {
+        this("(150 CC)", 8, true);
+    }
+
+    // initializes the following attributes for the construction of a 150 cc engine.
+    public OneHundredFiftyCCEngine(String name, int numOfCylinders, boolean isSingleOrDualCamShaft) {
+        super(name, numOfCylinders, isSingleOrDualCamShaft);
+    }
+
+    /**
+     * retrieves and returns the threshold value of a given engine's speed.
+     *
+     * @return the maximum allowable speed via a threshold value spec.
+     */
+    @Override
+    public double getSpeedThreshold() {
+
+        SPEED_THRESHOLD = 45.0;
+        return SPEED_THRESHOLD;
+
+    }
+
+    /**
+     * retrieves and returns the threshold value of a given engine's power.
+     *
+     * @return the maximum allowable power via a threshold value spec.
+     */
+    @Override
+    public double getPowerThreshold() {
+
+        POWER_THRESHOLD = 35.0;
+        return POWER_THRESHOLD;
+
+    }
+
+    /**
+     * represents a sequence of characters describing meaningful content (i.e., attributes).
+     *
+     * @return a concatenation of meaningful characters describing things like attributes.
+     */
+    @Override
+    public String toString() {
+
+        return       super.toString() +
+                ", Speed Threshold: " +
+                  getSpeedThreshold() +
+            " mph, Power Threshold: " +
+                  getPowerThreshold() +
+                                " mph";
+
+    }
+
+    /**
+     * checks if two objects are equal to one another.
+     *
+     * @param o the object being compared for equality with "this" object.
+     * @return true if both objects are equal and false if not.
+     */
+    @Override
+    public boolean equals(Object o) {
+
+        if(super.equals(o)) {
+            return true;
+        }
+
+        OneHundredFiftyCCEngine obj = (OneHundredFiftyCCEngine) o;
+
+        return SPEED_THRESHOLD - obj.SPEED_THRESHOLD <= 0.0001
+            && POWER_THRESHOLD - obj.POWER_THRESHOLD <= 0.0001;
+    }
+
+}
